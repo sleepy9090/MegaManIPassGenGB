742dff01c6c627fd8a11d92b9e601a4f53844b1c
Shawn M. Crawford <sleepy3d@gmail.com>
2019-04-09 11:33:20 -0400

    Initial Commit

---
+++ MegaManIPassGenGB/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace MegaManIPassGenGB
+{
+    partial class FormMegaManIPassGenGB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMegaManIPassGenGB));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxRollingCutter = new System.Windows.Forms.CheckBox();
+            this.checkBoxIceSlasher = new System.Windows.Forms.CheckBox();
+            this.checkBoxThunderBeam = new System.Windows.Forms.CheckBox();
+            this.checkBoxFireStorm = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBoxA1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxA2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxA3 = new System.Windows.Forms.CheckBox();
+            this.checkBoxA4 = new System.Windows.Forms.CheckBox();
+            this.checkBoxB4 = new System.Windows.Forms.CheckBox();
+            this.checkBoxB3 = new System.Windows.Forms.CheckBox();
+            this.checkBoxB2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxB1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxC4 = new System.Windows.Forms.CheckBox();
+            this.checkBoxC3 = new System.Windows.Forms.CheckBox();
+            this.checkBoxC2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxC1 = new System.Windows.Forms.CheckBox();
+            this.checkBoxD4 = new System.Windows.Forms.CheckBox();
+            this.checkBoxD3 = new System.Windows.Forms.CheckBox();
+            this.checkBoxD2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxD1 = new System.Windows.Forms.CheckBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxFireStorm);
+            this.groupBox1.Controls.Add(this.checkBoxThunderBeam);
+            this.groupBox1.Controls.Add(this.checkBoxIceSlasher);
+            this.groupBox1.Controls.Add(this.checkBoxRollingCutter);
+            this.groupBox1.Location = new System.Drawing.Point(12, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Weapon Acquired / Boss Defeated";
+            // 
+            // checkBoxRollingCutter
+            // 
+            this.checkBoxRollingCutter.AutoSize = true;
+            this.checkBoxRollingCutter.Location = new System.Drawing.Point(6, 19);
+            this.checkBoxRollingCutter.Name = "checkBoxRollingCutter";
+            this.checkBoxRollingCutter.Size = new System.Drawing.Size(137, 17);
+            this.checkBoxRollingCutter.TabIndex = 0;
+            this.checkBoxRollingCutter.Text = "Rolling Cutter / CutMan";
+            this.checkBoxRollingCutter.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIceSlasher
+            // 
+            this.checkBoxIceSlasher.AutoSize = true;
+            this.checkBoxIceSlasher.Location = new System.Drawing.Point(6, 42);
+            this.checkBoxIceSlasher.Name = "checkBoxIceSlasher";
+            this.checkBoxIceSlasher.Size = new System.Drawing.Size(126, 17);
+            this.checkBoxIceSlasher.TabIndex = 1;
+            this.checkBoxIceSlasher.Text = "Ice Slasher / IceMan";
+            this.checkBoxIceSlasher.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxThunderBeam
+            // 
+            this.checkBoxThunderBeam.AutoSize = true;
+            this.checkBoxThunderBeam.Location = new System.Drawing.Point(6, 65);
+            this.checkBoxThunderBeam.Name = "checkBoxThunderBeam";
+            this.checkBoxThunderBeam.Size = new System.Drawing.Size(149, 17);
+            this.checkBoxThunderBeam.TabIndex = 2;
+            this.checkBoxThunderBeam.Text = "Thunder Beam / ElecMan";
+            this.checkBoxThunderBeam.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFireStorm
+            // 
+            this.checkBoxFireStorm.AutoSize = true;
+            this.checkBoxFireStorm.Location = new System.Drawing.Point(6, 88);
+            this.checkBoxFireStorm.Name = "checkBoxFireStorm";
+            this.checkBoxFireStorm.Size = new System.Drawing.Size(122, 17);
+            this.checkBoxFireStorm.TabIndex = 3;
+            this.checkBoxFireStorm.Text = "Fire Storm / FireMan";
+            this.checkBoxFireStorm.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBoxD4);
+            this.groupBox2.Controls.Add(this.checkBoxD3);
+            this.groupBox2.Controls.Add(this.checkBoxD2);
+            this.groupBox2.Controls.Add(this.checkBoxD1);
+            this.groupBox2.Controls.Add(this.checkBoxC4);
+            this.groupBox2.Controls.Add(this.checkBoxC3);
+            this.groupBox2.Controls.Add(this.checkBoxC2);
+            this.groupBox2.Controls.Add(this.checkBoxC1);
+            this.groupBox2.Controls.Add(this.checkBoxB4);
+            this.groupBox2.Controls.Add(this.checkBoxB3);
+            this.groupBox2.Controls.Add(this.checkBoxB2);
+            this.groupBox2.Controls.Add(this.checkBoxB1);
+            this.groupBox2.Controls.Add(this.checkBoxA4);
+            this.groupBox2.Controls.Add(this.checkBoxA3);
+            this.groupBox2.Controls.Add(this.checkBoxA2);
+            this.groupBox2.Controls.Add(this.checkBoxA1);
+            this.groupBox2.Location = new System.Drawing.Point(211, 150);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(92, 114);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Password";
+            // 
+            // checkBoxA1
+            // 
+            this.checkBoxA1.AutoSize = true;
+            this.checkBoxA1.Location = new System.Drawing.Point(6, 24);
+            this.checkBoxA1.Name = "checkBoxA1";
+            this.checkBoxA1.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxA1.TabIndex = 0;
+            this.checkBoxA1.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxA2
+            // 
+            this.checkBoxA2.AutoSize = true;
+            this.checkBoxA2.Location = new System.Drawing.Point(27, 24);
+            this.checkBoxA2.Name = "checkBoxA2";
+            this.checkBoxA2.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxA2.TabIndex = 1;
+            this.checkBoxA2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxA3
+            // 
+            this.checkBoxA3.AutoSize = true;
+            this.checkBoxA3.Location = new System.Drawing.Point(48, 24);
+            this.checkBoxA3.Name = "checkBoxA3";
+            this.checkBoxA3.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxA3.TabIndex = 2;
+            this.checkBoxA3.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxA4
+            // 
+            this.checkBoxA4.AutoSize = true;
+            this.checkBoxA4.Location = new System.Drawing.Point(69, 24);
+            this.checkBoxA4.Name = "checkBoxA4";
+            this.checkBoxA4.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxA4.TabIndex = 3;
+            this.checkBoxA4.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxB4
+            // 
+            this.checkBoxB4.AutoSize = true;
+            this.checkBoxB4.Location = new System.Drawing.Point(69, 44);
+            this.checkBoxB4.Name = "checkBoxB4";
+            this.checkBoxB4.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxB4.TabIndex = 7;
+            this.checkBoxB4.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxB3
+            // 
+            this.checkBoxB3.AutoSize = true;
+            this.checkBoxB3.Location = new System.Drawing.Point(48, 44);
+            this.checkBoxB3.Name = "checkBoxB3";
+            this.checkBoxB3.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxB3.TabIndex = 6;
+            this.checkBoxB3.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxB2
+            // 
+            this.checkBoxB2.AutoSize = true;
+            this.checkBoxB2.Location = new System.Drawing.Point(27, 44);
+            this.checkBoxB2.Name = "checkBoxB2";
+            this.checkBoxB2.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxB2.TabIndex = 5;
+            this.checkBoxB2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxB1
+            // 
+            this.checkBoxB1.AutoSize = true;
+            this.checkBoxB1.Location = new System.Drawing.Point(6, 44);
+            this.checkBoxB1.Name = "checkBoxB1";
+            this.checkBoxB1.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxB1.TabIndex = 4;
+            this.checkBoxB1.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC4
+            // 
+            this.checkBoxC4.AutoSize = true;
+            this.checkBoxC4.Location = new System.Drawing.Point(69, 64);
+            this.checkBoxC4.Name = "checkBoxC4";
+            this.checkBoxC4.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxC4.TabIndex = 11;
+            this.checkBoxC4.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC3
+            // 
+            this.checkBoxC3.AutoSize = true;
+            this.checkBoxC3.Location = new System.Drawing.Point(48, 64);
+            this.checkBoxC3.Name = "checkBoxC3";
+            this.checkBoxC3.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxC3.TabIndex = 10;
+            this.checkBoxC3.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC2
+            // 
+            this.checkBoxC2.AutoSize = true;
+            this.checkBoxC2.Location = new System.Drawing.Point(27, 64);
+            this.checkBoxC2.Name = "checkBoxC2";
+            this.checkBoxC2.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxC2.TabIndex = 9;
+            this.checkBoxC2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC1
+            // 
+            this.checkBoxC1.AutoSize = true;
+            this.checkBoxC1.Location = new System.Drawing.Point(6, 64);
+            this.checkBoxC1.Name = "checkBoxC1";
+            this.checkBoxC1.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxC1.TabIndex = 8;
+            this.checkBoxC1.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxD4
+            // 
+            this.checkBoxD4.AutoSize = true;
+            this.checkBoxD4.Location = new System.Drawing.Point(69, 84);
+            this.checkBoxD4.Name = "checkBoxD4";
+            this.checkBoxD4.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxD4.TabIndex = 15;
+            this.checkBoxD4.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxD3
+            // 
+            this.checkBoxD3.AutoSize = true;
+            this.checkBoxD3.Location = new System.Drawing.Point(48, 84);
+            this.checkBoxD3.Name = "checkBoxD3";
+            this.checkBoxD3.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxD3.TabIndex = 14;
+            this.checkBoxD3.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxD2
+            // 
+            this.checkBoxD2.AutoSize = true;
+            this.checkBoxD2.Location = new System.Drawing.Point(27, 84);
+            this.checkBoxD2.Name = "checkBoxD2";
+            this.checkBoxD2.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxD2.TabIndex = 13;
+            this.checkBoxD2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxD1
+            // 
+            this.checkBoxD1.AutoSize = true;
+            this.checkBoxD1.Location = new System.Drawing.Point(6, 84);
+            this.checkBoxD1.Name = "checkBoxD1";
+            this.checkBoxD1.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxD1.TabIndex = 12;
+            this.checkBoxD1.UseVisualStyleBackColor = true;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(321, 238);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "&Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MegaManIPassGenGB.Properties.Resources.mm0;
+            this.pictureBox2.Location = new System.Drawing.Point(309, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 72);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MegaManIPassGenGB.Properties.Resources.mm;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 132);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormMegaManIPassGenGB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 273);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMegaManIPassGenGB";
+            this.Text = "MegaMan I PassGen (GB) - [sleepy9090]";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxFireStorm;
+        private System.Windows.Forms.CheckBox checkBoxThunderBeam;
+        private System.Windows.Forms.CheckBox checkBoxIceSlasher;
+        private System.Windows.Forms.CheckBox checkBoxRollingCutter;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxD4;
+        private System.Windows.Forms.CheckBox checkBoxD3;
+        private System.Windows.Forms.CheckBox checkBoxD2;
+        private System.Windows.Forms.CheckBox checkBoxD1;
+        private System.Windows.Forms.CheckBox checkBoxC4;
+        private System.Windows.Forms.CheckBox checkBoxC3;
+        private System.Windows.Forms.CheckBox checkBoxC2;
+        private System.Windows.Forms.CheckBox checkBoxC1;
+        private System.Windows.Forms.CheckBox checkBoxB4;
+        private System.Windows.Forms.CheckBox checkBoxB3;
+        private System.Windows.Forms.CheckBox checkBoxB2;
+        private System.Windows.Forms.CheckBox checkBoxB1;
+        private System.Windows.Forms.CheckBox checkBoxA4;
+        private System.Windows.Forms.CheckBox checkBoxA3;
+        private System.Windows.Forms.CheckBox checkBoxA2;
+        private System.Windows.Forms.CheckBox checkBoxA1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ MegaManIPassGenGB/Form1.cs
@@ -0,0 +1,254 @@
+﻿/**
+ *  @file           Form1.cs / FormMegaManIPassGenGB
+ *  @brief          Password Generator for the Nintendo Gameboy Game MegaMan I
+ *  @copyright      Shawn M. Crawford 2019
+ *  @date           04/09/2019
+ *
+ *  @remark Author  Shawn M. Crawford (sleepy9090)
+ *
+ *  @note           N/A
+ *
+ */
+using System;
+using System.Windows.Forms;
+
+namespace MegaManIPassGenGB
+{
+    public partial class FormMegaManIPassGenGB : Form
+    {
+        public FormMegaManIPassGenGB()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            disablePasswordCheckboxes();
+        }
+
+        private void disablePasswordCheckboxes()
+        {
+            checkBoxA1.Enabled = false;
+            checkBoxA2.Enabled = false;
+            checkBoxA3.Enabled = false;
+            checkBoxA4.Enabled = false;
+
+            checkBoxB1.Enabled = false;
+            checkBoxB2.Enabled = false;
+            checkBoxB3.Enabled = false;
+            checkBoxB4.Enabled = false;
+
+            checkBoxC1.Enabled = false;
+            checkBoxC2.Enabled = false;
+            checkBoxC3.Enabled = false;
+            checkBoxC4.Enabled = false;
+
+            checkBoxD1.Enabled = false;
+            checkBoxD2.Enabled = false;
+            checkBoxD3.Enabled = false;
+            checkBoxD4.Enabled = false;
+        }
+
+        private void clearPasswordCheckboxes()
+        {
+            checkBoxA1.Checked = false;
+            checkBoxA2.Checked = false;
+            checkBoxA3.Checked = false;
+            checkBoxA4.Checked = false;
+
+            checkBoxB1.Checked = false;
+            checkBoxB2.Checked = false;
+            checkBoxB3.Checked = false;
+            checkBoxB4.Checked = false;
+
+            checkBoxC1.Checked = false;
+            checkBoxC2.Checked = false;
+            checkBoxC3.Checked = false;
+            checkBoxC4.Checked = false;
+
+            checkBoxD1.Checked = false;
+            checkBoxD2.Checked = false;
+            checkBoxD3.Checked = false;
+            checkBoxD4.Checked = false;
+        }
+
+        private int getTotalChecked()
+        {
+            int totalChecked = 0;
+
+            if (checkBoxFireStorm.Checked)
+            {
+                totalChecked++;
+            }
+
+            if (checkBoxIceSlasher.Checked)
+            {
+                totalChecked++;
+            }
+
+            if (checkBoxRollingCutter.Checked)
+            {
+                totalChecked++;
+            }
+
+            if (checkBoxThunderBeam.Checked)
+            {
+                totalChecked++;
+            }
+
+            return totalChecked;
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            clearPasswordCheckboxes();
+
+            bool hasFireStorm = checkBoxFireStorm.Checked;
+            bool hasIceSlasher = checkBoxIceSlasher.Checked;
+            bool hasRollingCutter = checkBoxRollingCutter.Checked;
+            bool hasThunderBeam = checkBoxThunderBeam.Checked;
+
+            int totalChecked = getTotalChecked();
+
+            if (totalChecked == 4)
+            {
+                checkBoxA2.Checked = true;
+                checkBoxA3.Checked = true;
+                checkBoxB4.Checked = true;
+                checkBoxC2.Checked = true;
+                checkBoxC3.Checked = true;
+            }
+
+            if (totalChecked == 3)
+            {
+                if (hasIceSlasher && hasFireStorm && hasThunderBeam)
+                {
+                    checkBoxA2.Checked = true;
+                    checkBoxA3.Checked = true;
+                    checkBoxC1.Checked = true;
+                    checkBoxD2.Checked = true;
+                    checkBoxD3.Checked = true;
+                }
+                else if (hasRollingCutter && hasFireStorm && hasThunderBeam)
+                {
+                    checkBoxA3.Checked = true;
+                    checkBoxB2.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxB4.Checked = true;
+                    checkBoxC4.Checked = true;
+                }
+                else if (hasRollingCutter && hasIceSlasher && hasThunderBeam)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxA3.Checked = true;
+                    checkBoxC1.Checked = true;
+                    checkBoxC4.Checked = true;
+                    checkBoxD3.Checked = true;
+                }
+                else if (hasRollingCutter && hasIceSlasher && hasFireStorm)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxA2.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxC4.Checked = true;
+                    checkBoxD4.Checked = true;
+                }
+            }
+
+            if (totalChecked == 2)
+            {
+                if (hasIceSlasher && hasRollingCutter)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxA3.Checked = true;
+                    checkBoxA4.Checked = true;
+                    checkBoxB4.Checked = true;
+                    checkBoxD2.Checked = true;
+                }
+                else if (hasRollingCutter && hasFireStorm)
+                {
+                    checkBoxA2.Checked = true;
+                    checkBoxB2.Checked = true;
+                    checkBoxC3.Checked = true;
+                    checkBoxD1.Checked = true;
+                    checkBoxD3.Checked = true;
+                }
+                else if (hasRollingCutter && hasThunderBeam)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxA3.Checked = true;
+                    checkBoxB1.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxD3.Checked = true;
+                }
+                else if (hasIceSlasher && hasFireStorm)
+                {
+                    checkBoxA2.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxD2.Checked = true;
+                    checkBoxD3.Checked = true;
+                    checkBoxD4.Checked = true;
+                }
+                else if (hasIceSlasher && hasThunderBeam)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxB4.Checked = true;
+                    checkBoxC1.Checked = true;
+                    checkBoxD2.Checked = true;
+                    checkBoxD4.Checked = true;
+                }
+                else if (hasThunderBeam && hasFireStorm)
+                {
+                    checkBoxB2.Checked = true;
+                    checkBoxB4.Checked = true;
+                    checkBoxC1.Checked = true;
+                    checkBoxC3.Checked = true;
+                    checkBoxC4.Checked = true;
+                }
+            }
+
+            if (totalChecked == 1)
+            {
+                if (hasIceSlasher)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxA2.Checked = true;
+                    checkBoxB2.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxD4.Checked = true;
+                }
+                else if (hasRollingCutter)
+                {
+                    checkBoxA4.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxC4.Checked = true;
+                    checkBoxD3.Checked = true;
+                    checkBoxD4.Checked = true;
+                }
+                else if (hasFireStorm)
+                {
+                    checkBoxA1.Checked = true;
+                    checkBoxB1.Checked = true;
+                    checkBoxB2.Checked = true;
+                    checkBoxC4.Checked = true;
+                    checkBoxD2.Checked = true;
+                }
+                else if (hasThunderBeam)
+                {
+                    checkBoxA2.Checked = true;
+                    checkBoxA4.Checked = true;
+                    checkBoxB3.Checked = true;
+                    checkBoxD1.Checked = true;
+                    checkBoxD2.Checked = true;
+                }
+            }
+
+            if (totalChecked == 0)
+            {
+                checkBoxA3.Checked = true;
+                checkBoxB1.Checked = true;
+                checkBoxB3.Checked = true;
+                checkBoxC2.Checked = true;
+                checkBoxC4.Checked = true;
+            }
+        }
+    }
+}
